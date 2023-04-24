36c0bf474b6b6533201fc95c5f9c29a791ace907
anisia1204 <anisia.rosu@student.upt.ro>
2023-04-25 01:03:48 +0300

    created /save request mapping

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -45,7 +47,16 @@ public class EmployeeController {
 
 		return "employees/employee-form"; //returning the actual template that we want to use for this form
 	}
-}
+
+	@PostMapping("/save")// we pass in a model attribute(from the form) to the method which is the actual form data being passed in using the data binding
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+			//save the employee
+			employeeService.save(theEmployee);
+			//use a redirect to prevent duplicate submissions
+			return "redirect:/employees/list";
+		}
+	}
+
 
 
 
