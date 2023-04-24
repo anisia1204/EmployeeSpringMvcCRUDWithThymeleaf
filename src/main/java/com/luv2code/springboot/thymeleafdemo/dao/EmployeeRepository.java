ea600739acbc0af130de1e59c708ec17ca3a202a
anisia1204 <anisia.rosu@student.upt.ro>
2023-04-25 01:10:16 +0300

    added custom method to JPA Repository for sorting the employees in ascending order

--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -4,8 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
-	
+
+    //add a method to sort the employees by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+    //above, Spring Data JPA will parse the method name, will look for a specific format and pattern
+    // and it will create the appropriate query behind the scenes
+    // behind the scenes, Spring Data JPA will say "from Employee order by lastName asc"
 }
